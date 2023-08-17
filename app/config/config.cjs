30038878cd85637f3c8c21b1a1841acd2d5e538f
Irene Chigrinova <to.chigrinova@gmail.com>
2023-08-17 16:10:08 +0300

    env debug

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -7,11 +7,11 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
     port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || 'db',
     dialectOptions: {
       ssl: false,
     },
